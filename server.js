9a23fa2674a1c7de24e1be74d26a904b7e93ff58
Phill Mitchell-Matsuyama <phillicia.mitchell21@gmail.com>
2020-01-04 12:10:20 -0500

    latest

--- server.js
+++ server.js
@@ -12,26 +12,42 @@ const PORT = process.env.PORT || 8000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Arrays
+// =============================================================
+let reservations = [
+    {
+        id: 10001,
+        name: "Me",
+        email: "me@yahoo.com",
+        phone: "222-222-2222"
+    }
+];
+
 // Routes
 // ==============================================================
 
 // Home Route
 app.get("/", function(req, res){
-    res.send("This is the resturant page");
-    // res.sendFile(path.join(__dirname, "home.html"))
+    // res.send("This is the resturant page");
+    res.sendFile(path.join(__dirname, "home.html"))
 })
 
 // Table Route
 app.get("/tables", function(req, res){
     // res.send("This is the tables page");
-    // res.sendFile(path.join(__dirname, "tables.html"))
+    res.sendFile(path.join(__dirname, "tables.html"))
 })
 
 // Reserve Route
 app.get("/reserve", function(req, res){
-    res.send("This is the reserve page");
-    // res.sendFile(path.join(__dirname, "reserve.html"))
+    // res.send("This is the reserve page");
+    res.sendFile(path.join(__dirname, "reserve.html"))
 })
+
+// Display all reservations
+app.get("/api/reservations", function(req, res) {
+    return res.json(reservations);
+  });
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
